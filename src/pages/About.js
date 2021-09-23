3fd810c710b02f5301ad322f6becf1e453e9870e
coldpigli <piyushdas98@gmail.com>
2021-09-23 20:40:08 +0530

    added about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className = "section about-section">
+      <h1 className = "section-title">About Cocktails</h1>
+      <p>
+      Curabitizzle shut the shizzle up dizzle break it down nisi aliquam mollizzle. Suspendisse potenti. My shizz odio. Cool neque. Shizzlin dizzle orci. Crizzle mauris shit, interdizzle a, feugiat sit amizzle, we gonna chung izzle, hizzle. Pellentesque gravida. Vestibulizzle orci mi, volutpizzle yo, sagittis sed, fo shizzle mah nizzle fo rizzle, mah home g-dizzle semper, velizzle. Crizzle izzle ipsum. Aliquam volutpat felis shizznit its fo rizzle. Cras fo shizzle my nizzle ma nizzle dizzle purus crunk ornare. Shizzle my nizzle crocodizzle venenatizzle justo izzle lacus. Da bomb dope. Suspendisse dizzle placerizzle . Curabitizzle its fo rizzle ante. Go to hizzle pharetra, leo eu dapibus my shizz, ipsum felizzle things, funky fresh magna felizzle luctus the bizzle. Crunk a nisl. Class aptent taciti things izzle stuff torquent pimpin' conubia my shizz, pizzle crazy yippiyo. Aliquam shizzlin dizzle, own yo' nizzle for sure nonummy, nisl orci away owned, in sempizzle risus arcu mah nizzle sizzle.
+
+Sed ullamcorpizzle. Mah nizzle sagittizzle shut the shizzle up a go to hizzle. We gonna chung shut the shizzle up boom shackalack shit pimpin' faucibizzle tellivizzle luctus izzle ultrices posuere uhuh ... yih! Curae; Aenizzle vestibulum. Stuff habitant morbi crackalackin senectus izzle sure izzle malesuada shizznit ac izzle ghetto. Donizzle tempizzle hendrerizzle velit. Ass dang volutpat. Vivamus check it out fo shizzle, scelerisque mammasay mammasa mamma oo sa, dignissizzle a, go to hizzle brizzle, fo. Break it down elit. Its fo rizzle fermentizzle, est izzle ma nizzle aliquizzle, magna erat ultricies neque, dawg ullamcorpizzle mah nizzle dolor get down get down nibh. Shizzle my nizzle crocodizzle izzle shizznit, away mah nizzle, ornare that's the shizzle, vulputate nizzle, leo. Praesent purizzle get down get down, fo sizzle amizzle, interdum vehicula, tellivizzle malesuada, shiznit. Aenizzle we gonna chung fo shizzle shiz est gangsta tincidunt. Yippiyo quam. Hizzle ligula you son of a bizzle, fo shizzle my nizzle id, uhuh ... yih!, sodales sheezy, shit. Etizzle gravida.
+      </p>
+    </section>
   )
 }
 
