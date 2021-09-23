712136671ed986a6ed2a7f1aa743780b06ed6b04
coldpigli <piyushdas98@gmail.com>
2021-09-23 20:55:01 +0530

    set up contextProviders

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,16 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const {cocktails, loading} = useGlobalContext();
+
+  if(loading){
+    return <Loading/>
+  }
+  if(cocktails.length < 1){
+    return <h2 className = "section-title">
+      I'm sorry. We don't serve this here :(
+    </h2>
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
